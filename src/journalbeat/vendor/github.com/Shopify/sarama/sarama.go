ef4b205155357e2c2393d5dd846b2e7e96f0ba3a
Shubam Mehta <s26mehta@gmail.com>
2016-09-19 16:54:05 -0700

    Adding apache license and adding license header to source files

--- src/journalbeat/vendor/github.com/Shopify/sarama/sarama.go
+++ src/journalbeat/vendor/github.com/Shopify/sarama/sarama.go
@@ -1,4 +1,19 @@
 /*
+Copyright 2016 Planet Labs 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/*
 Package sarama is a pure Go client library for dealing with Apache Kafka (versions 0.8 and later). It includes a high-level
 API for easily producing and consuming messages, and a low-level API for controlling bytes on the wire when the high-level
 API is insufficient. Usage examples for the high-level APIs are provided inline with their full documentation.
